b93fd00c6bb849a6c92c9d59f73c3d87c6614f37
Ben Brandt <benbrandt22@gmail.com>
2022-12-07 01:08:31 -0600

    potentially useful string extensions... thought I'd use 'em today but didn't

---
+++ Core/Shared/Extensions/StringExtensions.cs
@@ -0,0 +1,17 @@
+﻿namespace Core.Shared.Extensions;
+
+public static class StringExtensions
+{
+    public static string IndentAllLines(this string input, int spaces)
+    {
+        var lines = input.Split(new[] { "\r\n", "\r", "\n" }, StringSplitOptions.None);
+        var prepend = new string(' ', spaces);
+        return string.Join(Environment.NewLine, lines.Select(line => $"{prepend}{line}"));
+    }
+    
+    public static string RemoveEmptyLines(this string input)
+    {
+        var lines = input.Split(new[] { "\r\n", "\r", "\n" }, StringSplitOptions.RemoveEmptyEntries);
+        return string.Join(Environment.NewLine, lines);
+    }
+}
